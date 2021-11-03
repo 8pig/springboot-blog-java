d7cfca240ae0e75294a931073032fb7c4c06ac9c
zhaozhou <zhou773@foxmail.com>
2021-11-03 19:49:47 +0800

    fix: 修复int类型错误

--- parent-blog/api-blog/src/main/java/com/zhou/blog/vo/ArticleVo.java
+++ parent-blog/api-blog/src/main/java/com/zhou/blog/vo/ArticleVo.java
@@ -1,12 +1,14 @@
 package com.zhou.blog.vo;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class ArticleVo {
-
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
     private String title;
